c04606ce4c12b35a9d50686907993f7489398101
akos-sereg <akos.sereg@gmail.com>
2015-07-19 11:16:08 +0200

    html escaping values

--- bootstrap-properties.js
+++ bootstrap-properties.js
@@ -22,34 +22,39 @@ PropertiesDialog.prototype.open = function(component, title) {
 	var config = component.configuration;
 
 	for (var i=0; i!=config.length; i++) {
+		var value = component[config[i].field];
+		if (config[i].htmlEscape != undefined && config[i].htmlEscape == true) {
+			value = htmlUnescape(value);
+		}
+
 		switch(config[i].type) {
 			case 'int':
 				this.configHtml += '<label for="props_'+config[i].field+'">' + config[i].name + ':</label>';
-				this.configHtml += '<input type="number" class="form-control" id="props_'+config[i].field+'" value="'+component[config[i].field]+'">';
+				this.configHtml += '<input type="number" class="form-control" id="props_'+config[i].field+'" value="'+value+'">';
 				break;
 
 			case 'string':
 				this.configHtml += '<label for="props_'+config[i].field+'">' + config[i].name + ':</label>';
-				this.configHtml += '<input type="text" class="form-control" id="props_'+config[i].field+'" value="'+component[config[i].field]+'">';
+				this.configHtml += '<input type="text" class="form-control" id="props_'+config[i].field+'" value="'+value+'">';
 				break;
 
 			case 'text':
 			    this.configHtml += '<label for="props_'+config[i].field+'">' + config[i].name + ':</label>';
-				this.configHtml += '<textarea class="form-control" rows="5" id="props_'+config[i].field+'">'+component[config[i].field]+'</textarea>';
+				this.configHtml += '<textarea class="form-control" rows="5" id="props_'+config[i].field+'">'+value+'</textarea>';
 				break;
 
 			case 'dropdown':
 				this.configHtml += '<label for="props_'+config[i].field+'">' + config[i].name + ':</label>';
 				this.configHtml += '<select id="props_'+config[i].field+'" class="form-control">';
 				for (var j=0; j!=config[i].options.length; j++) {
-					this.configHtml += '<option value="'+config[i].options[j].key+'"" '+((component[config[i].field] == config[i].options[j].key) ? 'SELECTED' : '')+'>'+config[i].options[j].value+'</option>';
+					this.configHtml += '<option value="'+config[i].options[j].key+'"" '+((value == config[i].options[j].key) ? 'SELECTED' : '')+'>'+config[i].options[j].value+'</option>';
 				}
 				this.configHtml += '</select>';
 				break;
 
 			case 'boolean':
 				this.configHtml += '<div class="checkbox">';
-				this.configHtml += '<label><input '+((component[config[i].field]) ? 'CHECKED' : '')+' type="checkbox" class="checkbox" id="props_'+config[i].field+'"> ' + config[i].name + '</label>';
+				this.configHtml += '<label><input '+(value == true ? 'CHECKED' : '')+' type="checkbox" class="checkbox" id="props_'+config[i].field+'"> ' + config[i].name + '</label>';
 				this.configHtml += '</div>';
 				break;
 		}
@@ -79,6 +84,10 @@ PropertiesDialog.prototype.saveProperties = function() {
 			);
 			validForm = false;
 		}
+
+		if (this.component.configuration[i].htmlEscape != undefined && this.component.configuration[i].htmlEscape == true) {
+			selectedValue = htmlEscape(selectedValue);
+		} 
 
 		this.component[this.component.configuration[i].field] = selectedValue;
 	}
@@ -113,4 +122,23 @@ PropertiesDialog.prototype.renderForm = function(containerDivId) {
 
   $('#' + containerDivId).append(content);
 
+}
+
+function htmlEscape(str) {
+    return String(str)
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+}
+
+// I needed the opposite function today, so adding here too:
+function htmlUnescape(value){
+    return String(value)
+        .replace(/&quot;/g, '"')
+        .replace(/&#39;/g, "'")
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&amp;/g, '&');
 }
